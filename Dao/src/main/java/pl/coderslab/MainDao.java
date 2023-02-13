1e5f02ddb06abf94ca0c18c038816b66a26ed179
Nicpon99 <Nicpon044@onet.pl>
2023-02-13 19:01:29 +0100

    complete UserDao

---
+++ Dao/src/main/java/pl/coderslab/MainDao.java
@@ -0,0 +1,14 @@
+package pl.coderslab;
+
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class MainDao {
+    public static void main(String[] args) {
+
+    }
+}
